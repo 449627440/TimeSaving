a18725a4df2ce647676da664b7ca7d0ac43cb598
ZhenjieYan <Ericyzj@outlook.com>
2019-05-15 00:27:40 +0800

    完善底部导航栏

--- app/src/main/java/com/swufe/timesaving/MainActivity.java
+++ app/src/main/java/com/swufe/timesaving/MainActivity.java
@@ -24,16 +24,14 @@ public class MainActivity extends AppCompatActivity {
 
         mBottomTabBar
                 .init(getSupportFragmentManager())//初始化方法，必须第一个调用；传入参数为V4包下的FragmentManager
-                .setImgSize(50,50)//设置ICON图片的尺寸
-                .setFontSize(8)//设置文字的尺寸
+                .setImgSize(30,30)//设置ICON图片的尺寸
+                .setFontSize(16)//设置文字的尺寸
                 .setTabPadding(4,6,10)//设置ICON图片与上部分割线的间隔、图片与文字的间隔、文字与底部的间隔
-                .setChangeColor(Color.DKGRAY,Color.RED)//设置选中的颜色、未选中的颜色
-                .addTabItem("首页", R.mipmap.ic_launcher, MainFragment.class)//设置文字、一张图片、fragment
-                .addTabItem("信息", R.mipmap.erweima, MainFragment.class)//设置文字、一张图片、fragment
-                .addTabItem("我的", R.mipmap.erweima, MainFragment.class)//设置文字、一张图片、fragment
+                .addTabItem("首页", R.drawable.main_logo, MainFragment.class)//设置文字、一张图片、fragment
+                .addTabItem("信息", R.drawable.message_logo, MainFragment.class)//设置文字、一张图片、fragment
+                .addTabItem("我的", R.drawable.mine_logo, MainFragment.class)//设置文字、一张图片、fragment
                 .isShowDivider(false)//设置是否显示分割线
                 .setTabBarBackgroundColor(Color.WHITE)//设置底部导航栏颜色
-                .setTabBarBackgroundResource(R.mipmap.ic_launcher)//设置底部导航栏的背景图片【与设置底部导航栏颜色方法不能同时使用，否则会覆盖掉前边设置的颜色】
                 .setOnTabChangeListener(new BottomTabBar.OnTabChangeListener() {
                     @Override
                     public void onTabChange(int position, String name, View view) {
