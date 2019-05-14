11a8e5aed953041c4dedbcc8df4b478834d5cc21
ZhenjieYan <Ericyzj@outlook.com>
2019-05-14 16:36:23 +0800

    添加bmob控件

--- app/src/main/java/com/swufe/timesaving/Welcome/RegisterActivity.java
+++ app/src/main/java/com/swufe/timesaving/Welcome/RegisterActivity.java
@@ -2,14 +2,26 @@ package com.swufe.timesaving.Welcome;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.jkb.vcedittext.VerificationCodeEditText;
 import com.swufe.timesaving.R;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText phone;
+    private EditText password;
+    private VerificationCodeEditText code;
+    private Button send;
+    private Button register;
+    private String sphone = " ";
+    private String spassword = " ";
+    private String scode = " ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
     }
 }
