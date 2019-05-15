c04af6efd78ce1822f09f9f72e20435d65335728
449627440 <449627440@qq.com>
2019-05-15 20:31:58 +0800

    修改了主页

---
+++ app/src/main/java/com/swufe/timesaving/Main/Welfare/WelfareActivity.java
@@ -0,0 +1,15 @@
+package com.swufe.timesaving.Main.Welfare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.swufe.timesaving.R;
+
+public class WelfareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welfare);
+    }
+}
