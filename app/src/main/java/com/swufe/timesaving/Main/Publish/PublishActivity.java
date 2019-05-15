8b80e41cb6bb3ea1b598bbd0d0fd3f4a4ce2e300
449627440 <449627440@qq.com>
2019-05-15 23:56:40 +0800

    将UI自适应与logo修改

---
+++ app/src/main/java/com/swufe/timesaving/Main/Publish/PublishActivity.java
@@ -0,0 +1,15 @@
+package com.swufe.timesaving.Main.Publish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.swufe.timesaving.R;
+
+public class PublishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish);
+    }
+}
