7a1029dcdf1c74dbeb900cddce1163a5653b7233
449627440 <449627440@qq.com>
2019-05-15 15:24:59 +0800

    mine

---
+++ app/src/main/java/com/swufe/timesaving/Mine/WalletActivity.java
@@ -0,0 +1,15 @@
+package com.swufe.timesaving.Mine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.swufe.timesaving.R;
+
+public class WalletActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallet);
+    }
+}
