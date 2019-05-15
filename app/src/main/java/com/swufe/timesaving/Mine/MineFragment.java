33776dcd7c50a21aa2ea87ec0754ba8104ea1a14
ZhenjieYan <Ericyzj@outlook.com>
2019-05-15 20:34:51 +0800

    更改wallet关联

--- app/src/main/java/com/swufe/timesaving/Mine/MineFragment.java
+++ app/src/main/java/com/swufe/timesaving/Mine/MineFragment.java
@@ -1,8 +1,10 @@
 package com.swufe.timesaving.Mine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -19,6 +21,7 @@ public class MineFragment extends Fragment {
     private View view;
     private TextView textView;
     private Toolbar toolbar;
+    private ConstraintLayout constraintLayout;
 
     @Nullable
     @Override
@@ -28,6 +31,15 @@ public class MineFragment extends Fragment {
         textView.setText((String)BmobUser.getObjectByKey("nickName"));
         toolbar=view.findViewById(R.id.toolbar2);
         toolbar.setTitle("");
+        constraintLayout=view.findViewById(R.id.constraintLayout);
+        constraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getContext(), WalletActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 
--- app/src/main/java/com/swufe/timesaving/Mine/WalletActivity.java
+++ app/src/main/java/com/swufe/timesaving/Mine/WalletActivity.java
@@ -1,15 +1,27 @@
 package com.swufe.timesaving.Mine;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.swufe.timesaving.R;
 
 public class WalletActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
+        toolbar=findViewById(R.id.toolbar3);
+        toolbar.setTitle("");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
