04da13897125fcba4a8a0eed6bf8b488745724c2
ZhenjieYan <Ericyzj@outlook.com>
2019-05-19 22:21:28 +0800

    添加多个网站

--- app/src/main/java/com/swufe/timesaving/Main/Task/TaskActivity.java
+++ app/src/main/java/com/swufe/timesaving/Main/Task/TaskActivity.java
@@ -1,15 +1,24 @@
 package com.swufe.timesaving.Main.Task;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.swufe.timesaving.R;
 
 public class TaskActivity extends AppCompatActivity {
 
+    private View view;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+
+
     }
 }
