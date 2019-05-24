459f508f105a06547905415f5e6a2c7316e4e65e
449627440 <449627440@qq.com>
2019-05-24 13:39:20 +0800

    个人信息页面

---
+++ app/src/main/java/com/swufe/timesaving/Main/Methods/PersonInfo/PersonInfoActivity.java
@@ -0,0 +1,15 @@
+package com.swufe.timesaving.Main.Methods.PersonInfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.swufe.timesaving.R;
+
+public class PersonInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_info);
+    }
+}
---
+++ app/src/main/java/com/swufe/timesaving/Mine/Info/MyInfoActivity.java
@@ -0,0 +1,15 @@
+package com.swufe.timesaving.Mine.Info;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.swufe.timesaving.R;
+
+public class MyInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_info);
+    }
+}
