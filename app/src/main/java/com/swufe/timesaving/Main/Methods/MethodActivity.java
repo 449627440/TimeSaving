d0d59cb827d4bb6d031a1a0f73c80cd4f1825552
449627440 <449627440@qq.com>
2019-05-24 11:22:24 +0800

    创建了方法页面

---
+++ app/src/main/java/com/swufe/timesaving/Main/Methods/MethodActivity.java
@@ -0,0 +1,15 @@
+package com.swufe.timesaving.Main.Methods;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.swufe.timesaving.R;
+
+public class MethodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_method);
+    }
+}
